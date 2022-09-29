2b109bc9fb36d7cf4b3aedba05dc1cb69acb2c2c
LaurenV <laurenvdl45@gmail.com>
2022-09-29 19:36:11 +0200

    feat: check winners and save selection

--- lab2-es6/js/card.js
+++ lab2-es6/js/card.js
@@ -37,6 +37,10 @@ export default class Card {
       // this.markDone(e.target);
       // call checkWinner() on the Bingo class
       // try to call the save() method on the Bingo class
+
+        this.markDone(e.target);
+        Bingo.checkWinner();
+        Bingo.save();
     });
   }
 }
