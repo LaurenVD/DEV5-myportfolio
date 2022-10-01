109d0a5bc41b5de22e287141de846a3547f0dbe7
LaurenV <laurenvdl45@gmail.com>
2022-10-01 12:12:33 +0200

    feat: load and check cards that are done from localstorage

--- lab2-es6/js/bingo.js
+++ lab2-es6/js/bingo.js
@@ -120,6 +120,13 @@ export default class Bingo {
       // JSON.parse() will convert the string [1, 7, 8] back to an array which you can loop
       // loop over the numbers 1, 7, 8 and mark those cards as done by adding the right CSS class
       // .bingo__card--done
+      
+      let cardsWon = JSON.parse(localStorage.getItem("bingo"));
+
+      for (let i = 0; i < cardsWon.length; i++) {
+          let card = document.querySelector(`[data-number="${cardsWon[i]}"]`);
+          card.classList.add("bingo__card--done");
+      }
     }
   }
 }
