b54a266f315444bcfdce8656cec932b4a10f6e8c
LaurenV <laurenvdl45@gmail.com>
2022-10-06 16:14:04 +0200

    feat: show meal picture when temperature is cold or warm

--- lab3-api/main.js
+++ lab3-api/main.js
@@ -1,4 +1,24 @@
 import './style.css'
 import Weather from "./src/Weather.js"
+import Meal from './src/Meal';
 
 const weather = new Weather("c77134dc503d435bb9763002220510");
+const meal = new Meal();
+
+// if weather is less than 10 degrees, show a cold meal
+// if weather is more than 10 degrees, show a warm meal
+
+const mealImage = document.querySelector(".meal__image");
+
+if (weather.temp > 10) {
+    meal.getMealByName("chicken").then((data) => {
+        console.log(data);
+        mealImage.src = data.strMealThumb;
+    });
+}
+else {
+    meal.getMealByName("salad").then((data) => {
+        console.log(data, mealImage);
+        mealImage.src = data.strMealThumb;
+    });
+}
